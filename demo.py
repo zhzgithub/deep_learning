b2b14520123cdb57befaf1f50931516f556922d8
zhz <zhz@email.com>
2017-10-22 20:01:45 +0800

    1

---
+++ demo.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+coefficients = np.array([[1], [-20], [25]])
+w = tf.Variable([0],dtype=tf.float32)
+x = tf.placeholder(tf.float32, [3,1])
+cost = x[0][0]*w**2 + x[1][0]*w + x[2][0] # (w-5)**2
+train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+init = tf.global_variables_initializer()
+
+
+session = tf.Session()
+session.run(init)
+print(session.run(w))
+'''
+with tf.Session() as session:
+	session.run(init)
+	print(session.run(w))
+'''
+
+for i in range(1000):
+	session.run(train, feed_dict={x:coefficients})
+print(session.run(w))
